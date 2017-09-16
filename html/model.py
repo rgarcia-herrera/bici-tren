36317466e3106e130e29779e8c30dc6814c0b892
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-09-16 11:44:14 -0500

    simplify, simplify!

--- html/model.py
+++ html/model.py
@@ -1,34 +1,86 @@
-from mongoengine import Document, connect, GeoPointField, FloatField, \
-    DateTimeField
+from mongoengine import Document, FloatField, \
+    DateTimeField, PointField, LineStringField, \
+    connect  # must import connect, used from without
 from datetime import datetime
-from math import sqrt
+from math import sqrt, sin, cos
 import utm
+
+from pprint import pprint
+
+
+def swap_coords(coords):
+    return [coords[1], coords[0]]
 
 
 class Bike(Document):
-    point = GeoPointField()
+    point = PointField()
     speed = FloatField(default=0)
-
+    heading = FloatField(default=0)
+    destination = PointField(required=False)
     stamp = DateTimeField(default=datetime.now)
+
+    def to_dict(self):
+        return {'point': self.point,
+                'speed': self.speed,
+                'heading': self.heading,
+                'destination': self.destination,
+                'stamp': self.stamp}
 
     def __str__(self):
         return "<bike %s %s @%sm/s>" % (self.id, self.point, self.speed)
 
-    def update(self, point):
-
+    def update(self, new_point):
         (x1, y1,
-         ZONE_NUMBER, ZONE_LETTER) = utm.from_latlon(*self.point)
+         ZONE_NUMBER,
+         ZONE_LETTER) = utm.from_latlon(*swap_coords(self.point))
 
-        self.point = point
+        self.point = new_point
 
         (x2, y2,
-         ZONE_NUMBER, ZONE_LETTER) = utm.from_latlon(*self.point)
+         ZONE_NUMBER,
+         ZONE_LETTER) = utm.from_latlon(*swap_coords(new_point))
 
         now = datetime.now()
         tdelta = now - self.stamp
         seconds = tdelta.total_seconds()
-        
+
         self.speed = sqrt((x2 - x1)**2 * (y2 - y1)**2) / seconds
+
+        try:
+            self.heading = (x2 - x1) / (y2 - y1)
+        except ZeroDivisionError:
+            self.heading = 0
+
         self.stamp = now
 
-        self.save()
+    def predicted_point(self):
+        (x1, y1,
+         ZONE_NUMBER,
+         ZONE_LETTER) = utm.from_latlon(
+             swap_coords(*self.point['coordinates']))
+
+        x2 = x1 + cos(self.heading) * self.speed
+        y2 = y1 + sin(self.heading) * self.speed
+
+        return swap_coords(utm.to_latlon(x2, y2,
+                                         ZONE_NUMBER,
+                                         ZONE_LETTER))
+
+    # def update_current_segment(self):
+    #     a = [[self.point[0],
+    #           self.point[1]],
+    #          [self.predicted_point()[0],
+    #           self.predicted_point()[1]]]
+    #     pprint(a)
+    #     self.current_segment = {'type': 'LineString',
+    #                             'coordinates': a}
+
+    # def route(self):
+    #     route = json.loads(
+    #         urllib2.urlopen(route_url % (self.point[0],
+    #                                      self.point[1],
+    #                                      self.destination[0],
+    #                                      self.destination[1])).read())
+    #     coords = route['features'][0]['geometry']['coordinates']
+    #     pprint(coords)
+    #     return coords
--- html/update.py
+++ html/update.py
@@ -4,11 +4,17 @@ import argparse
 parser = argparse.ArgumentParser(
     description='update bike agent')
 
+parser.add_argument('--lat', type=float, required=True,
+                    help='latitude')
+
 parser.add_argument('--lon', type=float, required=True,
                     help='longitude')
 
-parser.add_argument('--lat', type=float, required=True,
-                    help='latitude')
+parser.add_argument('--dlat', type=float, required=True,
+                    help='destination latitude')
+
+parser.add_argument('--dlon', type=float, required=True,
+                    help='destination longitude')
 
 parser.add_argument('--id', default='new',
                     help='bike id')
@@ -19,10 +25,15 @@ model.connect('mydb')
 
 
 if args.id == 'new':
-    b = model.Bike(point=(args.lon, args.lat))
-    b.save()    
+    b = model.Bike()
 else:
     b = model.Bike.objects.with_id(args.id)
-    b.update((args.lon, args.lat))
+
+
+b.point = [args.lon, args.lat]
+b.destination = [args.dlon, args.dlat]
+b.update(new_point=[args.lon, args.lat])
+
+b.save()
 
 print b
