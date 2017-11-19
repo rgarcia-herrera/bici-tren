112cb6559f50215b9be8df132a134ee52b6f1ae0
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2017-11-19 17:22:04 -0600

    update to new api

--- abm/flock_sim.py
+++ abm/flock_sim.py
@@ -1,6 +1,7 @@
 from time import sleep
 # from datetime import datetime
 import bike_agent as model
+from router import refine
 
 model.connect('mydb')
 
@@ -12,27 +13,28 @@ h = 0.0
 # init source, target, speed for this many bikes
 for n in range(1):
     b = model.Bike()
-    b.speed = 3
+    b.speed = 30
     b.random_ride(sw_lat=19.37625563272936,
                   ne_lat=19.48957309227922,
                   ne_lng=-99.04912948608398,
                   sw_lng=-99.22079086303712)
+    b.route = refine(b.route, 30)
+    b.save()
     sleep(1)
     print "creada %s" % b
 
 while model.Bike.objects.count() > 0:
     for b in model.Bike.objects.all():
-        b.step()
+        if b.got_there():
+            b.delete()
+        else:
+            b.step()
+            print b
         # if b.get_near_bikes(10000).count() > 1:
         #     flock = model.Flock(b.get_near_bikes(1000))
 
         #     if abs(b.heading - b.heading_to(flock.centroid)) < 0.8:
         #         b.update_route(flock.centroid)
         #         b.status = 'flocking'
-
-        print b
-
-        if b.got_there():
-            b.delete()
 
         sleep(0.5)
