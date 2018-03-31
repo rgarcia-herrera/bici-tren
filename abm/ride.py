271b77e1168e8feb77650d269bec5338dd5ba6a0
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2018-03-31 14:24:14 -0600

    random ride using new orm

--- abm/ride.py
+++ abm/ride.py
@@ -1,4 +1,4 @@
-import agent as model
+import bike_agent as model
 import argparse
 import json
 from LatLon import LatLon, Latitude, Longitude
@@ -14,8 +14,8 @@ parser.add_argument('--speed', default=3.0, type=float,
 args = parser.parse_args()
 
 
-model.db.bind(provider='sqlite', filename=':memory:', create_db=True)
-model.db.generate_mapping(create_tables=True)
+model.agent.db.bind(provider='sqlite', filename=':memory:', create_db=True)
+model.agent.db.generate_mapping(create_tables=True)
 
 route = json.loads(args.route.read())
 coords = [[c[0], c[1]]
@@ -28,8 +28,15 @@ end = LatLon(Latitude(coords[-1][0]),
              Longitude(coords[-1][1]))
 
 
-with model.orm.db_session:
-    b = model.Agent()
-    b.set_point(start)
-    b.set_destination(end)
+with model.agent.orm.db_session:
+    b = model.Bike()
     b.speed = args.speed
+    b.random_ride(ne_lat=19.461332069967366,
+                  ne_lng=-99.09204483032227,
+                  sw_lat=19.40467336236742,
+                  sw_lng=-99.17787551879884,
+                  min_len=8, max_len=10)
+
+    while not b.got_there():
+        b.step()
+        print len(b.route)
